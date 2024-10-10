58511534a39cea7f9300c8da2a8efd5312dbf436
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-10-10 10:22:14 +0200

    lex app setup tool is added

--- metagpt/tools/libs/__init__.py
+++ metagpt/tools/libs/__init__.py
@@ -4,27 +4,6 @@
 # @Author  : lidanyang
 # @File    : __init__.py
 # @Desc    :
-#
-# import os
-# import sys
-# from pathlib import Path
-#
-# import django
-#
-# LEX_APP_PACKAGE_ROOT = Path(__file__).resolve().parent.parent.as_posix()
-# PROJECT_ROOT_DIR = Path(os.getcwd()).resolve()
-# sys.path.append(LEX_APP_PACKAGE_ROOT)
-#
-# # The DJANGO_SETTINGS_MODULE has to be set to allow us to access django imports
-# os.environ.setdefault(
-#     "DJANGO_SETTINGS_MODULE", "lex.lex_app.settings"
-# )
-# os.environ.setdefault(
-#     "PROJECT_ROOT", PROJECT_ROOT_DIR.as_posix()
-# )
-# os.environ.setdefault("LEX_APP_PACKAGE_ROOT", LEX_APP_PACKAGE_ROOT)
-#
-# django.setup()
 
 from metagpt.tools.libs import (
     data_preprocess,
@@ -35,15 +14,53 @@ from metagpt.tools.libs import (
     email_login,
 )
 
-# from lex.lex_app.model_utils.parse_utils import ModelStructure
-# from lex.lex_app.model_utils.ModelStructureBuilder import ModelStructureBuilder
-# from lex.lex_app.model_utils.ModelRegistration import ModelRegistration
-# from lex.lex_app.lex_models.CalculationModel import CalculationModel
-# from lex.lex_app.lex_models.LexModel import LexModel
-# from lex.lex_app.logging.CalculationIDs import CalculationIDs
-# from lex.lex_app.logging.CalculationLog import CalculationLog
-# from lex.lex_app.auth_helpers import get_tokens_and_permissions, get_user_info, resolve_user
-# from lex.lex_app.LexLogger import LexLogger
+from metagpt.tools.tool_registry import register_tool
+
+
+@register_tool()
+def lex_app_setup():
+    """
+    Initializes the Django environment and sets up necessary paths for the project.
+
+    This function performs the following tasks:
+    1. Defines the paths to the application and project root directories.
+       - `LEX_APP_PACKAGE_ROOT`: The parent directory of the `lex_app` package.
+       - `PROJECT_ROOT_DIR`: The current working directory of the project.
+
+    2. Adds the `LEX_APP_PACKAGE_ROOT` to the system path (`sys.path`) to ensure
+       that modules from the parent package are accessible.
+
+    3. Sets the required environment variables:
+       - `DJANGO_SETTINGS_MODULE`: Specifies the settings module for the Django project.
+       - `PROJECT_ROOT`: The root directory of the project.
+       - `LEX_APP_PACKAGE_ROOT`: The root directory of the application package.
+
+    4. Calls `django.setup()` to initialize the Django framework, making it possible to
+       use Django ORM, models, and other features.
+
+    Note: This script must be run in a Django-compatible environment, and
+          the Django settings file must be properly configured.
+    """
+
+    import os
+    import sys
+    import django
+
+    LEX_APP_PACKAGE_ROOT = "<absolute_path_of_lex_app_from_venv>" #TODO: This should be fetched programmatically
+    PROJECT_ROOT_DIR = "<absolute_path_of_the_project_working_dir>" #TODO: This should be fetched programmatically
+    sys.path.append(LEX_APP_PACKAGE_ROOT)
+
+    # The DJANGO_SETTINGS_MODULE has to be set to allow us to access django imports
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "lex.lex_app.settings"
+    )
+    os.environ.setdefault(
+        "PROJECT_ROOT", PROJECT_ROOT_DIR
+    )
+    os.environ.setdefault("LEX_APP_PACKAGE_ROOT", LEX_APP_PACKAGE_ROOT)
+
+    django.setup()
+
 
 _ = (
     data_preprocess,
@@ -52,15 +69,5 @@ _ = (
     gpt_v_generator,
     web_scraping,
     email_login,
-    # ModelStructure,
-    # ModelStructureBuilder,
-    # ModelRegistration,
-    # CalculationModel,
-    # LexModel,
-    # CalculationIDs,
-    # CalculationLog,
-    # get_tokens_and_permissions,
-    # get_user_info,
-    # resolve_user,
-    # LexLogger,
+    lex_app_setup
 )  # Avoid pre-commit error
